162169c99a0a9330f2db3d7652ccc7391dba8e0c
paulomelila <paulovitor22@hotmail.com>
2017-09-20 12:00:58 -0700

    add assignment 2

---
+++ Assignments/Assignment 02/Assignment 02/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Assignment 02
+//
+//  Created by Paulo Melila on 2017-09-20.
+//  Copyright © 2017 Paulo Melila. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
